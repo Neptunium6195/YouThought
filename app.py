5c7a49b27ac60e77a3afd41335eb90e1b64285b1
Neptunium6195 <seratesarah@gmail.com>
2025-10-11 20:14:15 -0700

    made responses into a .json

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-import random, os
+import random, os, json
 app = Flask(__name__)
-with open('chat_history.txt', 'r') as f:
-    lines = f.readlines()
-
-responses = {
-    "procrastinate": [
-        "Why do today what you can put off until tomorrow?",
-        "Procrastination is the art of keeping up with yesterday."
-    ],
-    "distracted": [
-        "Focus is the key to success.",
-        "Stay on track and avoid distractions.",
-        "Put your phone or any other distractions in another room."
-    ],
-    "default": [
-        "I believe in you! What's your main task right now?"
-    ]
-}
 chatHistory = []
 
 def load_chat_history():
@@ -25,25 +8,39 @@ def load_chat_history():
         with open('chat_history.txt', 'r') as f:
             for line in f:
                 if line.startswith("user:"):
-                    chatHistory.append(("user", line[5:].strip()))
+                    chatHistory.insert(0, ("user", line[5:].strip()))
                 elif line.startswith("Bobot:"):
-                    chatHistory.append(("Bobot", line[6:].strip()))
+                    chatHistory.insert(0, ("Bobot", line[6:].strip()))
+    else:
+        print("chat_history.txt error")
     return chatHistory
-load_chat_history()
+
+def load_responses():
+    responses = {}
+    if os.path.exists('responses.json'):
+        with open('responses.json', 'r') as f2:
+            responses = json.load(f2)
+    else:
+        print("responses.json error")
+    return responses
+
+chatHistory = load_chat_history()
+responses = load_responses()
+
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
     global chatHistory
     if request.method == 'POST':
         user_message = request.form.get('user-input', '').strip()
-        chatHistory.append(("user", user_message))
+        chatHistory.insert(0, ("user", user_message))
         if "distract" in user_message or "focus" in user_message:
             bot_response = random.choice(responses["distracted"])
         elif "later" in user_message or "procrasinate" in user_message or "procrastinating" in user_message:
             bot_response = random.choice(responses["procrastinate"])
         else:
             bot_response = random.choice(responses["default"])
-        chatHistory.append(("Bobot", bot_response))
+        chatHistory.insert(0, ("Bobot", bot_response))
         with open('chat_history.txt', 'a') as f:
             f.write(f"user: {user_message}\n")
             f.write(f"Bobot: {bot_response}\n")
